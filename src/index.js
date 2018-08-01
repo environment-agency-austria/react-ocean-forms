f90998cf214012a13ace7b4356dee41afa374b25
Denis Masinovic <denis.masinovic@gmail.com>
2018-08-01 22:09:04 +0200

    Added FormButton

--- src/index.js
+++ src/index.js
@@ -23,3 +23,4 @@ export {
 export { default as FieldLine } from './FieldLine';
 export { default as ValidationSummary } from './ValidationSummary';
 export { default as Input } from './Input';
+export { default as FormButton } from './FormButton';
