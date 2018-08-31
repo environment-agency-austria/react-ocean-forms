0335bb264f1e77e5ab67ccc00c84218d2a3f6585
kappler <christoph.kappler@umweltbundesamt.at>
2018-08-31 10:36:02 +0200

    setup typescript

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,11 @@ const webpack = require('webpack');
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
+/**
+ * The source path
+ */
+const srcPath = path.resolve(__dirname, 'src');
+
 module.exports = {
   entry: './src/index.js',
   devtool: 'source-map',
@@ -15,16 +20,32 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx|mjs)$/,
-        include: path.resolve(__dirname, 'src'),
+        include: srcPath,
         exclude: /(node_modules|bower_components|build|coverage)/,
         use: {
           loader: 'babel-loader',
         },
       },
+      {
+        // This loader is used to transpile the .ts and .tsx files
+        // After that to js the output is transpiled
+        // using babel-loader.
+        test: /\.(tsx?)$/,
+        include: srcPath,
+        exclude: /(node_modules|bower_components|build|coverage)/,
+        use: [
+          'babel-loader', {
+            loader: 'ts-loader',
+            options: {
+              configFile: path.resolve(srcPath, 'tsconfig.build.json'),
+            },
+          },
+        ],
+      },
     ],
   },
   resolve: {
-    extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx']
+    extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx', '.ts', '.tsx'],
   },
   externals: {
     react: 'commonjs react',
