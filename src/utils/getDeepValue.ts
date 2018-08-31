1024189a8c727ca670aaf56b024470e44fd6047f
kappler <christoph.kappler@umweltbundesamt.at>
2018-08-31 10:42:05 +0200

    getDeepValue transitioned to typescript

---
+++ src/utils/getDeepValue.ts
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2018-present, Umweltbundesamt GmbH
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// Local type for better readability
+type TStringProp = {
+  // tslint:disable-next-line:no-any
+  [prop: string]: any;
+};
+
+/**
+ * Returns the 'deep' value of an object. For example
+ * you can provide the string 'foo.bar.baz' to get
+ * the value of the third nested object.
+ * @param name Name / property path
+ * @param object Object
+ */
+export default function getDeepValue<T, U extends TStringProp = TStringProp>(name: string, object: U): T | undefined {
+  return (name.split('.').reduce(
+    (o: U, i: string) => {
+      // Workaround for deep objects and
+      // 'cannot read property of undefined'
+      // - is there a better way?
+      if (o === undefined) { return undefined; }
+      if (o[i] === null) { return undefined; }
+
+      return o[i];
+    },
+    object));
+}
