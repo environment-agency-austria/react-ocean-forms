c839cb616e7c5a492380557749cbd15edab83a87
kappler <christoph.kappler@umweltbundesamt.at>
2018-08-31 10:37:20 +0200

    toArray transitioned to typescript

--- src/utils/toArray.ts
+++ src/utils/toArray.ts
@@ -9,9 +9,10 @@
  * Returns an array with the parameter as its only element if
  * the parameter is not an array. Otherwise it returns an array
  */
-export default function toArray(param) {
+export default function toArray<T>(param: T | T[]): T[] {
   if (!Array.isArray(param)) {
     return [param];
   }
+
   return param;
 }
