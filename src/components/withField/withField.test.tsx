228560f254be47e5b2654bdfbc1addaef52967a7
Denis Masinovic <denis.masinovic@gmail.com>
2018-10-14 15:55:24 +0200

    Added withField unit test

---
+++ src/components/withField/withField.test.tsx
@@ -0,0 +1,73 @@
+import * as React from 'react';
+
+import { shallow, ShallowWrapper } from 'enzyme';
+
+import { IFieldComponentFieldProps, IFieldComponentMeta, IFieldComponentProps } from './Field/Field.types';
+import { withField } from './withField';
+
+describe('withField', () => {
+  interface ISetupArgs {
+    props?: Partial<IFieldComponentProps>;
+  }
+
+  interface ISetupResult {
+    wrapper: ShallowWrapper;
+    renderProp(field: IFieldComponentFieldProps, meta: IFieldComponentMeta): JSX.Element;
+  }
+
+  const setup = ({
+    props,
+  }: Partial<ISetupArgs> = {}): ISetupResult => {
+    // tslint:disable-next-line:naming-convention
+    const TestComponent = (): JSX.Element => (<div id="test-component" />);
+    // tslint:disable-next-line:naming-convention
+    const WrappedComponent = withField(TestComponent);
+
+    const wrapper = shallow((
+      <WrappedComponent
+        name="mock-item"
+        label="Mock Item"
+        {...props}
+      />
+    ));
+
+    const renderProp = wrapper.prop('render');
+
+    return {
+      wrapper,
+      renderProp,
+    };
+  };
+
+  it('should render without error', () => {
+    const { wrapper } = setup();
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('render prop', () => {
+    it('should render without error', () => {
+      const { renderProp } = setup();
+      const wrapper = shallow(
+        renderProp(
+          {
+            disabled: false,
+            id: 'mock-item',
+            name: 'mock-item',
+            value: '',
+            onChange: jest.fn(),
+            onBlur: jest.fn(),
+          },
+          {
+            error: null,
+            isValidating: false,
+            plaintext: false,
+            stringFormatter: jest.fn(),
+            touched: false,
+            valid: true,
+          },
+        ),
+      );
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+});
