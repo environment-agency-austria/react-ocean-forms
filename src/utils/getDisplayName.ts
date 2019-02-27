e8a16a802a8dc3d1684e866c285fd460d9a494e7
Denis Masinovic <denis.masinovic@gmail.com>
2019-02-28 00:12:01 +0100

    Refactored typings of getDisplayName due to strict mode

--- src/utils/getDisplayName.ts
+++ src/utils/getDisplayName.ts
@@ -4,8 +4,11 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import React from 'react';
 import { stringHasValue } from './stringHasValue';
+
+interface IObjectWithDisplayName {
+  displayName?: string;
+}
 
 interface IObjectWithName {
   name?: string;
@@ -15,8 +18,10 @@ interface IObjectWithName {
  * Returns the display name of the wrapped component.
  */
 // tslint:disable-next-line:naming-convention
-export const getDisplayName = (wrappedComponent: React.ComponentType): string => {
-  if (stringHasValue(wrappedComponent.displayName)) { return wrappedComponent.displayName; }
+export const getDisplayName = (wrappedComponent: IObjectWithName | IObjectWithDisplayName | unknown): string => {
+  if (stringHasValue((<IObjectWithDisplayName>wrappedComponent).displayName)) {
+    return <string>(<IObjectWithDisplayName>wrappedComponent).displayName;
+  }
   if (stringHasValue((<IObjectWithName>wrappedComponent).name)) { return <string>(<IObjectWithName>wrappedComponent).name; }
 
   return 'Component';
