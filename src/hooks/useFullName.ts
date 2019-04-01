b4fe2c30d183e686469b02b87c5460c9b2772428
Denis Masinovic <denis.masinovic@gmail.com>
2019-04-01 22:56:01 +0200

    Added useFullName hook

---
+++ src/hooks/useFullName.ts
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2019-present, Umweltbundesamt GmbH
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import { useFormContext } from './useFormContext';
+
+/**
+ * Returns the full name of the field, consisting of
+ * the current `FormContext.fieldPrefix` and the `name` of the field.
+ * @param name Name of the field
+ */
+export function useFullName(name: string): string {
+  const formContext = useFormContext();
+  return formContext.fieldPrefix !== null ? formContext.fieldPrefix.concat('.', name) : name;
+}
