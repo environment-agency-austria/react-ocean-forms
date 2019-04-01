11d82a9a48a77ba56b04a9d37c8fe565ab11b373
Denis Masinovic <denis.masinovic@gmail.com>
2019-04-01 22:58:20 +0200

    Refactored global export handling

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
-export * from './components/FieldError';
-export * from './components/FieldGroup';
-export * from './components/FieldLine';
-export * from './components/Form';
-export * from './components/FormButton';
-export * from './components/FormContext';
-export * from './components/FormText';
-export * from './components/Input';
-export * from './components/ValidationSummary';
-export * from './components/withField';
-export * from './components/withForm';
-export * from './components/withValidation';
-
+export * from './components';
 export * from './utils';
 export * from './validators';
+export * from './hooks';
