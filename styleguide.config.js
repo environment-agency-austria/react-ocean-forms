369beb59265c4b3c13c7b282cfd5602d10a783e3
Denis Masinovic <denis.masinovic@gmail.com>
2020-03-27 20:26:55 +0100

    Updated dependencies

--- styleguide.config.js
+++ styleguide.config.js
@@ -5,27 +5,27 @@ module.exports = {
   title: 'react-ocean-forms',
   pagePerSection: true,
   skipComponentsWithoutExample: true,
-  propsParser: require('react-docgen-typescript').withCustomConfig('./tsconfig.json', { skipPropsWithoutDoc: false }).parse,
+  // propsParser: require('react-docgen-typescript').withCustomConfig('./tsconfig.json', { skipPropsWithoutDoc: false }).parse,
   moduleAliases: {
     'react-ocean-forms': 'C:\\source\\github\\react-ocean-forms\\src',
   },
   getComponentPathLine(componentPath) {
-    const componentName = path.basename(componentPath, '.tsx')
-    return `import { ${componentName} } from 'react-ocean-forms';`
+    const componentName = path.basename(componentPath, '.tsx');
+    return `import { ${componentName} } from 'react-ocean-forms';`;
   },
   sections: [
     {
       name: 'Introduction',
-      content: 'docs/introduction.md'
+      content: 'docs/introduction.md',
     },
     {
       name: 'Changelog',
-      content: 'CHANGELOG.md'
+      content: 'CHANGELOG.md',
     },
     {
       name: 'Components',
       components: 'src/components/**/*.tsx',
-      sectionDepth: 1
+      sectionDepth: 1,
     },
     {
       name: 'Hooks',
@@ -33,56 +33,58 @@ module.exports = {
       sections: [
         {
           name: 'useField',
-          content: 'src/hooks/useField/useField.md'
+          content: 'src/hooks/useField/useField.md',
         },
         {
           name: 'useFormContext',
-          content: 'src/hooks/useFormContext/useFormContext.md'
+          content: 'src/hooks/useFormContext/useFormContext.md',
         },
         {
           name: 'useFormEventListener',
-          content: 'src/hooks/useFormEventListener/useFormEventListener.md'
+          content: 'src/hooks/useFormEventListener/useFormEventListener.md',
         },
         {
           name: 'useValidation',
-          content: 'src/hooks/useValidation/useValidation.md'
+          content: 'src/hooks/useValidation/useValidation.md',
         },
       ],
-    }
+    },
   ],
   theme: {
     fontFamily: {
-      base: '-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;'
+      base:
+        '-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;',
     },
   },
   styles: {
     StyleGuide: {
       logo: {
-        'background-color': '#343A40'
-      }
+        'background-color': '#343A40',
+      },
     },
     Logo: {
       logo: {
-        'color': '#FFF'
-      }
+        color: '#FFF',
+      },
     },
     Heading: {
       heading: {
         'font-weight': '300',
-      }
+      },
     },
     TabButton: {
       button: {
-        'color': '#1673B1',
+        color: '#1673B1',
         'border-bottom': '1px #1673B1 solid',
         'padding-bottom': '0px',
-      }
+      },
     },
     Playground: {
       preview: {
-        'font-family': '-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;',
-      }
-    }
+        'font-family':
+          '-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;',
+      },
+    },
   },
   ribbon: {
     url: 'https://github.com/environment-agency-austria/react-ocean-forms',
@@ -96,15 +98,15 @@ module.exports = {
           options: {
             compilerOptions: {
               noEmit: false,
-              jsx: "react",
-            }
+              jsx: 'react',
+            },
           },
           exclude: /node_modules/,
         },
-      ]
+      ],
     },
     resolve: {
-      extensions: [".tsx", ".ts"]
+      extensions: ['.tsx', '.ts'],
     },
-  }
+  },
 };
